a557d3860dd1b7c372fce40e96f9173831bcd2ab
Soumyajit Paria <soumyajitparia2005@gmail.com>
2024-11-24 22:30:43 +0530

    initialize React project with Vite, Tailwind CSS, and MUI setup

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Books from "./pages/Books/Books";
+import Characters from "./pages/Characters/Characters";
+import Spells from "./pages/Spells/Spells";
+import Houses from "./pages/Houses/Houses";
+import Home from "./pages/Home/Home";
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <div className="p-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books />} />
+          <Route path="/characters" element={<Characters />} />
+          <Route path="/spells" element={<Spells />} />
+          <Route path="/houses" element={<Houses />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
